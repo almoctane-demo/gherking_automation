09b93e4ee43e09db4e5276c0d90d4a5c9afff5f7
Spencer Bruce <spencer.bruce@hpe.com>
2018-01-18 17:34:34 +0200

    Rename

--- src/test/java/com/microfocus/adm/octane/gherkinautomation/ChromeLiveChatTest.java
+++ src/test/java/com/microfocus/adm/octane/gherkinautomation/ChromeLiveChatTest.java
@@ -1,6 +1,5 @@
 package com.microfocus.adm.octane.gherkinautomation;
 
-import com.sun.xml.internal.ws.api.FeatureListValidatorAnnotation;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
