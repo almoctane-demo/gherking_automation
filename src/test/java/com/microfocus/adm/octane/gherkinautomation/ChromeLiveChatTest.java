1e91d1734c898ba13f6f273f5e38d4d9f7287284
Spencer Bruce <spencer.bruce@hpe.com>
2018-01-18 17:22:17 +0200

    Fix pom version

--- src/test/java/com/microfocus/adm/octane/gherkinautomation/ChromeLiveChatTest.java
+++ src/test/java/com/microfocus/adm/octane/gherkinautomation/ChromeLiveChatTest.java
@@ -1,5 +1,6 @@
 package com.microfocus.adm.octane.gherkinautomation;
 
+import com.sun.xml.internal.ws.api.FeatureListValidatorAnnotation;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
